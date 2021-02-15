e9951d1416d5b4bf163977ab71f0d5eb4e14090e
forsakemenot <thanapon.asd@gmail.com>
2021-02-15 11:38:50 +0700

    installation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          johny Johny Yes papa
         </p>
         <a
           className="App-link"
