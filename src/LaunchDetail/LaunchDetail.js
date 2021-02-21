57ce078de492e1c2df9e2d5633a969a6c6de95e5
forsakemenot <thanapon.asd@gmail.com>
2021-02-21 22:31:23 +0700

    LaunchDetail

--- src/LaunchDetail/LaunchDetail.js
+++ src/LaunchDetail/LaunchDetail.js
@@ -22,36 +22,36 @@ function LaunchDetail() {
         [],
     );
     return (
-        <div className="body_launch w-full">
+        <div className="body_launch w-full h-screen">
             {launchs.links &&
                 <>
                     <>
-                        <div className="flex h-screen container mx-auto items-center justify-center ">
-                            <div className="flex flex-row justify-center">
-                                <div className="flex justify-center items-center mr-20">
+                        <div className="flex h-screen container mx-auto items-center justify-center">
+                            <div className="flex lg:flex-row justify-center sm:flex-col detail_container">
+                                <div className="flex justify-center items-center lg:mr-20 mb-4 ">
                                     <img className="justify-center" src={launchs.links.mission_patch_small}></img>
                                 </div>
-                                <div className='flex flex-col justify-center items-center w-6/12'>
-                                    <div className="flex w-full">
-                                        <p className="text-3xl mb-2 uppercase text-left">Mission : {launchs.mission_name}</p>
+                                <div className='flex flex-col justify-center items-center w-full'>
+                                    <div className="flex w-full text_title">
+                                        <p className="text-3xl mb-2 uppercase text-left ">Mission : {launchs.mission_name}</p>
                                     </div>
-                                    <div className="flex w-full">
+                                    <div className="flex w-full status_bar">
                                         <div className="bg-blue-500 w-52 rounded-md justify-center flex mb-2">
                                             <p className="uppercase text-white">
                                                 Rocket Name : {launchs.rocket.rocket_id}
                                             </p>
                                         </div>
                                         {launchs.details
-                                            ? <div className="ml-5 bg-green-500 w-52 rounded-md justify-center flex mb-2"><p className="uppercase text-white">LAUNCH : SUCCESS</p></div>
-                                            : <div className="ml-5 bg-red-500 w-52 rounded-md justify-center flex mb-2"><p className="uppercase text-white">LAUNCH : FAILURE</p></div>
+                                            ? <div className="lg:ml-5 md:ml-5 bg-green-500 w-52 rounded-md justify-center flex mb-2"><p className="uppercase text-white">LAUNCH : SUCCESS</p></div>
+                                            : <div className="lg:ml-5 md:ml-5 bg-red-500 w-52 rounded-md justify-center flex mb-2"><p className="uppercase text-white">LAUNCH : FAILURE</p></div>
                                         }
                                     </div>
                                     <div className="flex w-full flex-col">
                                         {launchs.details
-                                            ? <p className="text-xl text-left">{launchs.details}</p>
-                                            : <p className="text-xl text-left text-gray-400 blink">- NO ADDITIONAL DESCRIPTION ABOUT THIS MISSION -</p>
+                                            ? <p className="text-xl text-left detail_text">{launchs.details}</p>
+                                            : <p className="text-xl text-left text-gray-400 blink detail_text">- NO ADDITIONAL DESCRIPTION ABOUT THIS MISSION -</p>
                                         }
-                                        <div><button onClick={handleClick} class="w-2/12 bg-yellow-400 hover:bg-yellow-300 transition text-white font-bold py-2 px-4 rounded mt-8 back_btn">BACK</button></div>
+                                        <div className="back_button_container"><button onClick={handleClick} class="w-2/12 bg-yellow-400 hover:bg-yellow-300 transition text-white font-bold py-2 px-4 rounded mt-8 back_btn">BACK</button></div>
                                     </div>
                                 </div>
 
